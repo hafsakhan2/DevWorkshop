a337dd9071b890274c493ea692c72deac0c35846
hafsakhan2 <143122568+hafsakhan2@users.noreply.github.com>
2023-11-15 13:52:20 -0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        // Replace <Your Name> with your actual name
+        System.out.println("Hello, my name is <Your Name>");
+    }
+}
